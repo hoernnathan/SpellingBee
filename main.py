63b25339790a3d047bd15f25137f70909c005489
hoernnathan <38138495+hoernnathan@users.noreply.github.com>
2023-03-22 11:13:46 -0400

    Added comments to code and updated word and letter lists

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
 import random
+"""
+f = open("wordlist2.txt", "r")
+g = open("wordlist4.txt", "a")
+with open("wordlist3.txt") as w:
+    items = f.readlines()
+    items2 = w.readlines()
+    #items3 = g.readlines()
+    print(items)
+    for item in items2:
+        if item not in items:
+            g.write(item)
+        #inFile = False
+        #for word in w:
+        #    if word == item:
+        #        inFile = True
+        #        break
+        #if not inFile:
+        #    g.write(item)
+f.close()
+g.close()
+"""
 
-def CreateBoard(letters):
+
+def PrintBoard(letters):
     middle_letter = letters[0]
     other_letters = letters[1:7]
     print("           _______          ")
@@ -17,36 +39,38 @@ def CreateBoard(letters):
     print("         \         /        ")
     print("          \_______/         ")
 
+
 def AddWords(letters, showBoard):
-    #print(letters)
     if showBoard:
-        CreateBoard(letters)
+        PrintBoard(letters)
     words = []
     with open("wordlist2.txt") as w:
+
+        # parse the word list file to add all possible words with the given letters (that contain the middle letter) to a list
         for line in w:
-            flag = True
+            letterInPuzzle = True
             middle_letter_count = 0
             for letter in line:
                 if letter == '\n':
                     break
                 if letter not in letters:
-                    flag = False
+                    letterInPuzzle = False
                     break
                 if letter == letters[0]:
                     middle_letter_count += 1
-            if flag and middle_letter_count > 0:
+            if letterInPuzzle and middle_letter_count > 0:
                 if len(line) >= 5:
                     words.append(line[0:-1])
     w.close()
-    #for word in words:
-        #print(word)
     return words, letters
+
 
 def PlayGame(words, letters, totalPoints):
     option = ""
     myWords = []
     lettered_list = letters
-    #print(totalPoints)
+
+    # set the point values for each ranking
     pointTotals = {
         "GENIUS": round(totalPoints*0.7),
         "AMAZING": round(totalPoints*0.5),
@@ -60,8 +84,11 @@ def PlayGame(words, letters, totalPoints):
     }
     userPoints = 0
     currentLevel = 'BEGINNER'
+
+    # play puzzle until user quits or obtains all the words
     while option != 'Q' and len(myWords) != len(words):
-        #print("Enter a word by typing it below. Enter S to shuffle the letters in the hive, W to view your words, or H to bring up a help menu, or G when you are ready to give up.")
+
+        # check if the user reached a new ranking
         for key in pointTotals:
             if userPoints >= pointTotals[key]:
                 if key == currentLevel:
@@ -70,6 +97,10 @@ def PlayGame(words, letters, totalPoints):
                 currentLevel = key
                 break
         option = input("Enter word:")
+
+        # check if the user entered a command: S, B, H, W, P, or Q
+
+        # S: shuffle the board
         if option == 'S':
             lettered_list = []
             middle_letter = letters[0]
@@ -78,9 +109,13 @@ def PlayGame(words, letters, totalPoints):
             lettered_list.append(middle_letter)
             for item in other_letters:
                 lettered_list.append(item)
-            CreateBoard(lettered_list)
+            PrintBoard(lettered_list)
+
+        # B: print the board
         elif option == 'B':
-            CreateBoard(lettered_list)
+            PrintBoard(lettered_list)
+
+        # H: display the help menu
         elif option == 'H':
             print("\nHere is the help guide for Spelling Bee!")
             print("Rules:")
@@ -96,11 +131,15 @@ def PlayGame(words, letters, totalPoints):
             print("  - W to view the words you have obtained")
             print("  - Q to quit this puzzle")
             print("  - H to bring up this help menu\n")
+
+        # W: display all the user's obtained words
         elif option == 'W':
             print("\nYou have found", len(myWords), "words:")
             for word in myWords:
                 print(word)
             print("")
+
+        # P: display the points for each ranking, and the user's total points and ranking
         elif option == 'P':
             print("\nTotal number of words:", len(words))
             print("Total number of points:", totalPoints)
@@ -111,34 +150,49 @@ def PlayGame(words, letters, totalPoints):
             for key, value in pointTotals.items():
                 print(" ", key, value)
             print("")
+
+        # Q: quit the puzzle
         elif option == 'Q':
             break
+
+        # check the user didn't already obtain the word they entered
         elif option in myWords:
             print("You've already gotten this word.")
+
+        # check if the user's word matches a word in the word list
         elif option.upper() in words:
             letter_list = []
-            # checks if the user's word contains all the letters, this is a pangram
+
+            # count the number of distinct letters in the user's word
             for letter in option:
                 if letter not in letter_list:
                     letter_list.append(letter)
+
+            # checks if the user's word contains all the letters, this is a pangram and the user earns 7 extra points
             if len(letter_list) == 7:
-                print("PANGRAM! +", len(option)+7, "points")
+                print("PANGRAM! +{} points".format(len(option)+7))
                 userPoints += len(option)+7
+
+            # otherwise, the user earns 1 point for a 4-letter word and n points for an n-length word > 4
             elif len(option) == 4:
-                print("Good! +", 1, "point")
+                print("Good! +1 point")
                 userPoints += 1
             elif len(option) <= 6:
-                print("Nice! +", len(option), "points")
+                print("Nice! +{} points".format(len(option)))
                 userPoints += len(option)
             else:
-                print("Awesome! +", len(option), "points")
+                print("Awesome! +{} points".format(len(option)))
                 userPoints += len(option)
             myWords.append(option)
             myWords.sort()
         else:
             print("Not accepted. Either this word is not in the word list, contains letters not in the hive, is too short, or it does not contain the middle letter.")
+
+        # checks if the user obtained all the words in the puzzle
         if len(myWords) == len(words):
             print("YOU FOUND ALL THE WORDS. YOU ARE A QUEEN BEE!")
+
+    # print any words in the puzzle that the user did not obtain
     if len(myWords) != len(words):
         print("Here are the words that you missed:")
         words.sort()
@@ -152,14 +206,17 @@ def PlayGame(words, letters, totalPoints):
                 counter += 1
         print("")
 
+
 def RandomGen():
     lettersList = ['A', 'B', 'C', 'D', 'E',
              'F', 'G', 'H', 'I', 'J',
              'K', 'L', 'M', 'N', 'O',
-             'P', 'Q', 'R', 'T', 'U',
+             'P', 'Q', 'R', 'S', 'T', 'U',
              'V', 'W', 'X', 'Y', 'Z']
     acceptableLetters = False
     randLetList = []
+
+    # generate a set of seven unique letters until all the following conditions hold
     while not acceptableLetters:
         randLetList = random.sample(lettersList, 7)
         vowelList = ['A','E','I','O','U']
@@ -167,17 +224,40 @@ def RandomGen():
         for letter in randLetList:
             if letter in vowelList:
                 vowelCount += 1
-        if 'E' in randLetList and 'R' in randLetList:
+
+        # check for no I, N, G
+        #if 'I' in randLetList and 'N' in randLetList and 'G' in randLetList:
+        #    acceptableLetters = False
+
+        # check for no E and D
+        #elif 'E' in randLetList and 'D' in randLetList:
+        #    acceptableLetters = False
+
+        # check for no S
+        if 'S' in randLetList:
             acceptableLetters = False
+
+        # check for no E and R
+        elif 'E' in randLetList and 'R' in randLetList:
+            acceptableLetters = False
+
+        # check for U with Q
         elif 'Q' in randLetList and 'U' not in randLetList:
             acceptableLetters = False
+
+        # check vowel count
         elif vowelCount < 1 or vowelCount > 3:
             acceptableLetters = False
+
         else:
             words, letters = AddWords(randLetList, False)
-            if len(words) < 15 or len(words) > 85:
+
+            # check acceptable word count
+            if len(words) < 17 or len(words) > 81:
                 acceptableLetters = False
             else:
+
+                # check that a pangram exists in this set of letters
                 pangram = False
                 for word in words:
                     letArr = []
@@ -190,16 +270,18 @@ def RandomGen():
                     acceptableLetters = True
     return randLetList
 
+
 if __name__ == '__main__':
     print("Welcome to Spelling Bee! Press P to start a new puzzle. Press Q to quit.")
     option = ""
-    # TODO: make the input file dynamic for users
     with open("letterlist.txt") as l:
         while option != 'Q':
             option = input("Enter:")
             if option == 'P':
                 l.seek(0)
                 content = l.readlines()
+
+                # format the output of the file to make it more readable for the user when selecting a puzzle to play
                 for line in content:
                     lets = line[3:-1]
                     arr = []
@@ -214,6 +296,8 @@ if __name__ == '__main__':
                     print("( middle letter:", line[3], ")")
                 validPuzzle = False
                 randomPuzzle = False
+
+                # input validation to check the user entered a valid puzzle number or R for a random puzzle
                 while not validPuzzle:
                     puzzleNum = input("\nWhich puzzle would you like to play? (type the number, or R for a random set of letters): ")
                     if puzzleNum == 'R':
@@ -221,9 +305,13 @@ if __name__ == '__main__':
                         validPuzzle = True
                     elif puzzleNum.isdigit() and 0 < int(puzzleNum) <= len(content):
                         validPuzzle = True
+
+                # call a function to generate random letters if random puzzle
                 if randomPuzzle:
                     randLetList = RandomGen()
                     words, letters = AddWords(randLetList, True)
+
+                # otherwise, pass the letters for the appropriate puzzle in to AddWords
                 else:
                     c = content[int(puzzleNum)-1]
                     let = c[3:]
@@ -232,6 +320,8 @@ if __name__ == '__main__':
                         letterArray.append(letter)
                     words, letters = AddWords(letterArray, True)
                 totalPoints = 0
+
+                # count the total number of points in this puzzle
                 for word in words:
                     if len(word) == 4:
                         totalPoints += 1
@@ -239,7 +329,8 @@ if __name__ == '__main__':
                         totalPoints += len(word)
                     else:
                         letter_list = []
-                        # checks if the user's word contains all the letters, this is a pangram
+                        
+                        # checks if the word contains all 7 letters, this is a pangram
                         for letter in word:
                             if letter not in letter_list:
                                 letter_list.append(letter)
@@ -247,7 +338,7 @@ if __name__ == '__main__':
                             totalPoints += len(word) + 7
                         else:
                             totalPoints += len(word)
-                    #print("Total points in this puzzle:", totalPoints)
+
                 PlayGame(words, letters, totalPoints)
                 print("Would you like to play again with a different set of letters? Press P to play again, Q to quit.")
     l.close()
